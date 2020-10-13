7857c76a99957234e347ffc308358e937f2c82d4
Kaushal <kaushal.sharma@anu.edu.au>
2020-10-13 18:23:17 +1100

    Updated files in src folder with minor changes in header and new directory for data

--- src/gen_data_path.py
+++ src/gen_data_path.py
@@ -1,6 +1,6 @@
 import os
 import glob
-import _init_paths
+import src._init_paths
 
 
 def gen_caltech_path(root_path):
@@ -21,7 +21,7 @@ def gen_data_path(root_path):
     mot_path = 'MOT17/images/train'
     real_path = os.path.join(root_path, mot_path)
     seq_names = [s for s in sorted(os.listdir(real_path)) if s.endswith('SDP')]
-    with open('/home/yfzhang/PycharmProjects/fairmot/src/data/mot17.half', 'w') as f:
+    with open('../src/data/mot17.half', 'w') as f:
         for seq_name in seq_names:
             seq_path = os.path.join(real_path, seq_name)
             seq_path = os.path.join(seq_path, 'img1')
@@ -38,7 +38,7 @@ def gen_data_path_mot17_val(root_path):
     mot_path = 'MOT17/images/train'
     real_path = os.path.join(root_path, mot_path)
     seq_names = [s for s in sorted(os.listdir(real_path)) if s.endswith('SDP')]
-    with open('/home/yfzhang/PycharmProjects/fairmot2/src/data/mot17.val', 'w') as f:
+    with open('../src/data/mot17.val', 'w') as f:
         for seq_name in seq_names:
             seq_path = os.path.join(real_path, seq_name)
             seq_path = os.path.join(seq_path, 'img1')
@@ -55,7 +55,7 @@ def gen_data_path_mot17_emb(root_path):
     mot_path = 'MOT17/images/train'
     real_path = os.path.join(root_path, mot_path)
     seq_names = [s for s in sorted(os.listdir(real_path)) if s.endswith('SDP')]
-    with open('/home/yfzhang/PycharmProjects/fairmot2/src/data/mot17.emb', 'w') as f:
+    with open('../src/data/mot17.emb', 'w') as f:
         for seq_name in seq_names:
             seq_path = os.path.join(real_path, seq_name)
             seq_path = os.path.join(seq_path, 'img1')
@@ -69,5 +69,5 @@ def gen_data_path_mot17_emb(root_path):
 
 
 if __name__ == '__main__':
-    root = '/data/yfzhang/MOT/JDE'
+    root = 'C:/Users/Kaushal/Desktop/FairMOT'
     gen_data_path_mot17_emb(root)
